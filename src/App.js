b8bd973e4c949aac8ba0b39dd6fff51c396b4da3
oiojin831 <oiojin831@gmail.com>
2021-05-30 13:45:23 +0900

    Add list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ListItem, UnorderedList } from '@chakra-ui/react';
 
 export default function App() {
-  return <ChakraProvider>Hello world</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <UnorderedList>
+        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+        <ListItem>Consectetur adipiscing elit</ListItem>
+        <ListItem>Integer molestie lorem at massa</ListItem>
+        <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+      </UnorderedList>
+    </ChakraProvider>
+  );
 }
