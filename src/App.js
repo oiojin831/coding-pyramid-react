4008e808861fe784323fa9ce10ec00ad676966bc
oiojin831 <oiojin831@gmail.com>
2021-05-30 13:53:42 +0900

    print products

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ListItem, UnorderedList } from '@chakra-ui/react';
 
+const products = [
+  {
+    title: '맥스 97',
+    category: '신발',
+    price: 180000
+  },
+  {
+    title: '맥스 95',
+    category: '신발',
+    price: 200000
+  },
+  {
+    title: '검정 티셔츠',
+    category: '옷',
+    price: 30000
+  },
+  {
+    title: '자전거',
+    category: '운동기기',
+    price: 200000
+  }
+];
+
 export default function App() {
+  function printProduct(product) {
+    return (
+      <ListItem>
+        {`${product.title} - ${product.category} - ${product.price}원`}
+      </ListItem>
+    );
+  }
   return (
     <ChakraProvider>
-      <UnorderedList>
-        <ListItem>Lorem ipsum dolor sit amet</ListItem>
-        <ListItem>Consectetur adipiscing elit</ListItem>
-        <ListItem>Integer molestie lorem at massa</ListItem>
-        <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-      </UnorderedList>
+      <UnorderedList>{products.map(printProduct)}</UnorderedList>
     </ChakraProvider>
   );
 }
