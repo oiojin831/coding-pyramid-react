9c323de50fc6797450d5930295634174e84a119a
oiojin831 <oiojin831@gmail.com>
2021-05-30 13:41:30 +0900

    Install Chakra ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <ChakraProvider>Hello world</ChakraProvider>;
 }
